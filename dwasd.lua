91cdb10eee65549d725e7eb092fb4688079996c1
Winnable-Hub <vxnyishere@gmail.com>
2025-04-03 18:51:19 +0700

    Create dwasd.lua

---
+++ dwasd.lua
@@ -0,0 +1,29 @@
+function checklevel(wdas)
+yeahs = wdas:GetAttributes()
+for i2,v2 in yeahs do
+if i2 == "Level" then
+return v2
+end
+end
+end
+
+weapon = {}
+
+for i,v in pairs(game:GetService("Players").LocalPlayer.leaderstats.Inventory.Weapons:GetChildren()) do
+if string.find(v.Name, _G.Settings.Weapon_Name) and checklevel(v) == _G.Settings.Weapon_Rank_Remove then
+table.insert(weapon, v.Name)
+end
+end
+
+local args = {
+    [1] = {
+        [1] = {
+            ["Action"] = "Sell",
+            ["Event"] = "WeaponAction",
+            ["Name"] = weapon[1]
+        },
+        [2] = "\n"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("BridgeNet2"):WaitForChild("dataRemoteEvent"):FireServer(unpack(args))
